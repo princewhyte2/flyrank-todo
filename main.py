6cb1d26d2de1c6b6aef24e9935cb4373a08859c1
princewhyte2 <princewhyte02@gmail.com>
2026-07-21 17:07:18 +0100

    Stage 5: Swagger UI

--- main.py
+++ main.py
@@ -25,26 +25,26 @@ tasks = [{
 ]
 
 
-@app.get("/")
+@app.get("/", summary='Read root')
 def read_root():
   return { "name": "Task API", "version": "1.0", "endpoints": ["/tasks"] }
 
-@app.get("/health")
+@app.get("/health", summary='Health Status')
 def health_status():
   return {"status": "ok"}
 
-@app.get("/tasks")
+@app.get("/tasks", summary='Get all Tasks')
 def tasks_list():
   return {"tasks":tasks}
 
-@app.get("/tasks/{task_id}")
+@app.get("/tasks/{task_id}", summary='Get task by Id')
 def single_task(task_id):
   for task in tasks :
     if task["id"] == int(task_id):
       return {"task":task}
   raise HTTPException(status_code=404 , detail = "Task " + task_id + " not found")
 
-@app.post("/tasks",status_code=status.HTTP_201_CREATED)
+@app.post("/tasks",status_code=status.HTTP_201_CREATED, summary='Create a new Task')
 def create_task(task: Task, response:Response):
   if not task.title:
     response.status_code = status.HTTP_400_BAD_REQUEST
@@ -58,7 +58,7 @@ def create_task(task: Task, response:Response):
   tasks.append(new_task)
   return {"task":new_task}
 
-@app.put("/tasks/{task_id}")
+@app.put("/tasks/{task_id}",summary='Update an existing Task')
 def update_task(task_id: int , task:Task):
   if task.title is None and task.done is None:
     raise HTTPException(status_code=400 , detail = "Empty/invalid body")
@@ -71,7 +71,7 @@ def update_task(task_id: int , task:Task):
       return {"task": tasks[index]}
   raise HTTPException(status_code=404 , detail = "Unknown id")
 
-@app.delete("/tasks/{task_id}",status_code=status.HTTP_204_NO_CONTENT)
+@app.delete("/tasks/{task_id}",status_code=status.HTTP_204_NO_CONTENT,summary='Delete an existing task')
 def delete_task(task_id:int):
   for index,task_item in enumerate(tasks):
     if task_id == task_item["id"]:
